62e864dc016a2ad6972c554d2b8fbdda3f3fe808
Abhi Paul <abhishekpaul0055@gmail.com>
2018-01-18 01:01:43 +0530

    Updated UserResolver in User model

--- database/migrations/2017_09_03_114713_create_audits_table.php
+++ database/migrations/2017_09_03_114713_create_audits_table.php
@@ -37,6 +37,7 @@ class CreateAuditsTable extends Migration
                 $table->string('url')->nullable();
                 $table->ipAddress('ip_address')->nullable();
                 $table->string('user_agent')->nullable();
+                $table->string('tags')->nullable();
                 $table->timestamps();
             });
     }
