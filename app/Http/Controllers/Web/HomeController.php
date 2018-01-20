9d95ff10e436bb560cee385f25a74c229a943d48
Samir Maikap <maikap.samir@gmail.com>
2018-01-20 12:38:15 +0530

    Bug fixes

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Assessment;
+use App\Models\Award;
+use App\Models\Department;
 use App\Models\Organization;
+use App\Models\Quiz;
 use App\Models\Subscription;
+use App\Models\Ticket;
+use App\Models\TicketAssignment;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -72,14 +78,79 @@ class HomeController extends Controller
 
     public function removeaccount(){
         DB::beginTransaction();
+
+        $tcount=0;
+        $qcount=0;
+        $amcount=0;
+        $awardcount=0;
+
+
         if(session('role')=='learner'){
             $learner = Learner::withTrashed()->where('id', Auth::user()->account_id)->first();
             $subscription=Subscription::where('account_id',Auth::user()->account_id)->first();
             $user=User::where('account_id',Auth::user()->account_id)->first();
+            $assessment=Assessment::where('learner_id',Auth::user()->account_id)->get();
+            $award=Award::where('learner_id',Auth::user()->account_id)->get();
+            $quiz=Quiz::where('learner_id',Auth::user()->account_id)->get();
+            $tickets=Ticket::withTrashed()->where('learner_id',Auth::user()->account_id)->get();
+
+            if(count($assessment) > 0){
+                foreach ($assessment as $am){
+                    $am->forceDelete();
+                    $amcount++;
+                }
+            }
+            else{
+                $amcount=1;
+            }
+
+            if(count($award) > 0){
+                foreach ($award as $aw){
+                    $aw->forceDelete();
+                    $awardcount++;
+                }
+            }
+            else{
+                $awardcount=1;
+            }
+
+            if(count($quiz) > 0){
+                foreach ($quiz as $qz){
+                    $qz->forceDelete();
+                    $qcount++;
+                }
+            }
+            else{
+                $qcount=1;
+            }
+
+            if(count($tickets) > 0){
+                foreach ($tickets as $tk){
+                    $assignment=TicketAssignment::where('ticket_id',$tk->id)->get();
+                    if(count($assignment) > 0){
+                        foreach ($assignment as $as){
+                            $as->forceDelete();
+                        }
+                        $tk->forceDelete();
+                        $tcount++;
+                    }
+                    else{
+                        $tk->forceDelete();
+                        $tcount++;
+                    }
+
+                }
+            }
+            else{
+                $tcount=1;
+            }
+
+
+
             if(!empty($subscription->subscription_id)){
                 app('App\Http\Controllers\Web\SubscriptionController')->cancel($subscription->subscription_id);
             }
-            if($subscription->forceDelete() && $user->forceDelete() && $learner->forceDelete()){
+            if($subscription->forceDelete() && $user->forceDelete() && $learner->forceDelete() && $tcount > 0 && $qcount > 0 && $awardcount > 0 && $amcount > 0){
                 DB::commit();
 
                 return redirect()->intended(url('logout'));
@@ -91,25 +162,99 @@ class HomeController extends Controller
         }
         elseif(session('role')=='organization'){
             $count=0;
+            $depcount=0;
+
             $organization = Organization::withTrashed()->where('id',Auth::user()->account_id)->first();
             $learners=$organization->learners()->get();
+            $departments=Department::where('organization_id',Auth::user()->account_id)->get();
             $subscription=Subscription::where('account_id',Auth::user()->account_id)->first();
             $user=User::where('account_id',Auth::user()->account_id)->first();
+
+
             if(!empty($subscription->subscription_id)){
                 app('App\Http\Controllers\Web\SubscriptionController')->cancel($subscription->subscription_id);
             }
 
             if(count($learners) > 0 ){
                 foreach ($learners as $learner){
-                    $learner->forceDelete();
-                    $count++;
+                    $assessment=Assessment::where('learner_id',$learner->id)->get();
+                    $award=Award::where('learner_id',$learner->id)->get();
+                    $quiz=Quiz::where('learner_id',$learner->id)->get();
+                    $tickets=Ticket::withTrashed()->where('learner_id',$learner->id)->get();
+
+                    if(count($assessment) > 0){
+                        foreach ($assessment as $am){
+                            $am->forceDelete();
+                            $amcount++;
+                        }
+                    }
+                    else{
+                        $amcount=1;
+                    }
+
+                    if(count($award) > 0){
+                        foreach ($award as $aw){
+                            $aw->forceDelete();
+                            $awardcount++;
+                        }
+                    }
+                    else{
+                        $awardcount=1;
+                    }
+
+                    if(count($quiz) > 0){
+                        foreach ($quiz as $qz){
+                            $qz->forceDelete();
+                            $qcount++;
+                        }
+                    }
+                    else{
+                        $qcount=1;
+                    }
+
+                    if(count($tickets) > 0){
+                        foreach ($tickets as $tk){
+                            $assignment=TicketAssignment::where('ticket_id',$tk->id)->get();
+                            if(count($assignment) > 0){
+                                foreach ($assignment as $as){
+                                    $as->forceDelete();
+                                }
+                                $tk->forceDelete();
+                                $tcount++;
+                            }
+                            else{
+                                $tk->forceDelete();
+                                $tcount++;
+                            }
+
+                        }
+                    }
+                    else{
+                        $tcount=1;
+                    }
+
+                    if($amcount > 0 && $qcount > 0 && $awardcount > 0 && $tcount > 0){
+                        $learner->forceDelete();
+                        $count++;
+                    }
+
                 }
             }
             else{
                 $count=1;
             }
 
-            if($organization->forceDelete() && $subscription->forceDelete() && $user->forceDelete() && $count > 0){
+            if(count($departments) > 0){
+                foreach ($departments as $dep){
+                    $dep->forceDelete();
+                    $depcount++;
+                }
+            }
+            else{
+                $depcount=1;
+            }
+
+            if($organization->forceDelete() && $subscription->forceDelete() && $user->forceDelete() && $count > 0 && $depcount > 0){
                 DB::commit();
                 return redirect()->intended(url('logout'));
             }
