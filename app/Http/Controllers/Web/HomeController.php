5bbaaa7536b8567dbc0e433da382d987baa65c33
Samir Maikap <maikap.samir@gmail.com>
2018-01-21 12:13:44 +0530

    Bug fixes

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -38,7 +38,7 @@ class HomeController extends Controller
             return redirect()->intended('dashboard');
 
         }else{
-            redirect()->intended('login')->withErrors(['Incorrect email or password']);
+            return redirect()->intended('login')->withErrors(['Incorrect email or password']);
         }
     }
 
