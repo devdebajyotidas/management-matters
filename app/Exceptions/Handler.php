b1628b0ad4c35f20bcb509a036107115b8897611
Samir Maikap <maikap.samir@gmail.com>
2018-01-21 12:05:18 +0530

    Bug fixes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,6 +44,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        dd($request);
 //        return response()->view('errors.500', [], 500);
         return parent::render($request, $exception);
     }
