fc299581fd32a90afc44fea829c161bb42f3357e
Abhi Paul <abhishekpaul0055@gmail.com>
2018-01-29 19:07:36 +0530

    Updated menu for admin

--- resources/views/includes/main-menu.blade.php
+++ resources/views/includes/main-menu.blade.php
@@ -6,6 +6,7 @@
                     @if($role=="admin")
                         <li @if($page=='dashboard') class="tab-current" @endif><a href="{{url('/dashboard')}}" class="sticon ti-home"><span>Dashboard</span></a></li>
                         <li @if($page=='learnings') class="tab-current" @endif><a href="{{url('/learnings')}}" class="sticon ti-blackboard"><span>Learnings</span></a></li>
+                        <li @if($page=='cost') class="tab-current" @endif><a href="{{url('/cost/edit')}}" class="sticon ti-stats-down"><span>Cost of Not</span></a></li>
                         <li @if($page=='organizations') class="tab-current" @endif><a href="{{url('/organizations')}}" class="sticon ti-briefcase"><span>Organizations</span></a></li>
                         <li @if($page=='learners') class="tab-current" @endif><a href="{{url('/learners')}}" class="sticon ti-user"><span>Learners</span></a></li>
                         <li @if($page=='quiz') class="tab-current" @endif><a href="{{url('/quiz')}}" class="sticon ti-light-bulb"><span>Quiz</span></a></li>
