17868e313873458ed53711dd0a6be1045ae41dd2
Abhi Paul <abhishekpaul0055@gmail.com>
2018-02-05 20:04:25 +0530

    Updated Assessments API

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1097,8 +1097,19 @@
 
             @if($role == 'learner')
 
-            var cost = JSON.parse("{{ json_encode($cost) }}");
-            console.log(cost);
+
+            var parsed = JSON.parse('{!! json_encode($cost) !!}');
+            console.log(parsed);
+
+            var cost = [];
+            var date = [];
+            var result = $.parseJSON('{!! json_encode($cost) !!}');
+            $.each(result, function(k, v) {
+                date.push(moment(k).format("MM/DD/YYYY"));
+                cost.push(v);
+            });
+
+            console.log(cost,date);
 
             Highcharts.chart('chart', {
 
@@ -1109,11 +1120,18 @@
                 subtitle: {
                     text: ''
                 },
-
+                xAxis: {
+                    title: {
+                        text: 'Date'
+                    },
+//                    categories: [1,2,3,4,5,6,7,8,9,10]
+                    categories: date
+                },
                 yAxis: {
                     title: {
                         text: 'Total Impact($)'
-                    }
+                    },
+                    categories: cost
                 },
                 legend: {
                     layout: 'vertical',
@@ -1131,21 +1149,23 @@
                 },
 
                 series: [{
-                    name: 'Installation',
-                    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-                }, {
-                    name: 'Manufacturing',
-                    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-                }, {
-                    name: 'Sales & Distribution',
-                    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-                }, {
-                    name: 'Project Development',
-                    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-                }, {
-                    name: 'Other',
-                    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-                }],
+                    name: 'Cost',
+                    data: cost
+                }
+//                , {
+//                    name: 'Manufacturing',
+//                    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+//                }, {
+//                    name: 'Sales & Distribution',
+//                    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+//                }, {
+//                    name: 'Project Development',
+//                    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+//                }, {
+//                    name: 'Other',
+//                    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+//                }
+                ],
 
                 responsive: {
                     rules: [{
