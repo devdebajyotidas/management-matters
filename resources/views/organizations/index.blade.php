fb37746937c60fbe5228e796a782190cc3647ce4
Abhishek Paul <abhishekpaul0055@gmail.com>
2018-04-16 12:34:56 +0530

    Bug fixes

--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -353,31 +353,31 @@
                         {
                             extend: 'copyHtml5',
                             exportOptions: {
-                                columns: [ 0, 1, 2, 3, 4, 5]
+                                columns: [ 0, 1, 2, 3, 4]
                             }
                         },
                         {
                             extend: 'excelHtml5',
                             exportOptions: {
-                                columns: [ 0, 1, 2, 3, 4, 5]
+                                columns: [ 0, 1, 2, 3, 4]
                             }
                         },
                         {
                             extend: 'pdfHtml5',
                             exportOptions: {
-                                columns: [ 0, 1, 2, 3, 4, 5]
+                                columns: [ 0, 1, 2, 3, 4]
                             }
                         },
                         {
                             extend: 'csv',
                             exportOptions: {
-                                columns: [ 0, 1, 2, 3, 4, 5]
+                                columns: [ 0, 1, 2, 3, 4]
                             }
                         },
                         {
                             extend: 'print',
                             exportOptions: {
-                                columns: [ 0, 1, 2, 3, 4, 5]
+                                columns: [ 0, 1, 2, 3, 4]
                             }
                         }
                     ]
