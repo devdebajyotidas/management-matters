445bc92ba774812cda085c2d0287ee4d7dbf9a8e
Samir Maikap <maikap.samir@gmail.com>
2019-05-22 10:42:52 +0530

    Migrate attachment command update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\MigrateAttachments;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Log;
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-         Commands\QuotesSchedule::class
+         Commands\QuotesSchedule::class,
+         MigrateAttachments::class,
     ];
 
     /**
